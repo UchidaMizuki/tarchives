e6200bfeac4f7296f20e0aeba280d135f73f6faf
Uchida Mizuki <uchidamizuki@vivaldi.net>
2025-05-18 09:06:09 +0900

    air format

--- R/tar_read_archive.R
+++ R/tar_read_archive.R
@@ -8,12 +8,12 @@
 #'
 #' @export
 tar_read_archive <- function(
-    name,
-    package,
-    pipeline,
-    branches = NULL,
-    meta = NULL,
-    store = targets::tar_config_get("store")
+  name,
+  package,
+  pipeline,
+  branches = NULL,
+  meta = NULL,
+  store = targets::tar_config_get("store")
 ) {
   name <- targets::tar_deparse_language(substitute(name))
   tar_read_archive_raw(
@@ -30,21 +30,22 @@ tar_read_archive <- function(
 #'
 #' @export
 tar_read_archive_raw <- function(
-    name,
-    package,
-    pipeline,
-    branches = NULL,
-    meta = NULL,
-    store = targets::tar_config_get("store")
+  name,
+  package,
+  pipeline,
+  branches = NULL,
+  meta = NULL,
+  store = targets::tar_config_get("store")
 ) {
   store <- tar_archive_store(
     package = package,
     pipeline = pipeline,
     store = store
   )
-  meta <- meta %||% targets::tar_meta(
-    store = store
-  )
+  meta <- meta %||%
+    targets::tar_meta(
+      store = store
+    )
   targets::tar_read_raw(
     name = name,
     branches = branches,
