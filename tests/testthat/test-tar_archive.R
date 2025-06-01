6daf661906370c295d05d501afbcd1dc89c7b0cb
Uchida Mizuki <uchidamizuki@vivaldi.net>
2025-06-01 23:42:08 +0900

    Update test-tar_archive.R

--- tests/testthat/test-tar_archive.R
+++ tests/testthat/test-tar_archive.R
@@ -7,14 +7,6 @@ targets::tar_test("tar_archive_script() works", {
   expect_true(fs::file_exists(script))
 })
 
-targets::tar_test("tar_archive_store() works", {
-  store <- tar_archive_store(
-    package = "tarchives",
-    pipeline = "example-model"
-  )
-  expect_true(fs::is_dir(store))
-})
-
 targets::tar_test("tar_archive() works", {
   # targets::tar_outdated() for archived targets works
   tar_outdated_example_model <- tar_archive(
@@ -47,4 +39,10 @@ targets::tar_test("tar_archive() works", {
   )
   expect_vector(tar_outdated_example_model(), character(), 0)
   expect_vector(tar_outdated_example_plot(), character(), 0)
+
+  store <- tar_archive_store(
+    package = "tarchives",
+    pipeline = "example-model"
+  )
+  expect_true(fs::is_dir(store))
 })
